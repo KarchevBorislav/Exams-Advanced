42fed44c726ed8fbe7b1390ccfc2a15c1f2629ef
KarchevBorislav <bkarchev@web.de>
2024-01-21 10:10:07 +0100

    Update OffroadChallenge.java

--- StackAndQueues/OffroadChallenge.java
+++ StackAndQueues/OffroadChallenge.java
@@ -1,25 +1,26 @@
 package Exams.StackAndQueues;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class OffroadChallenge {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // initial fuel
-        int[] fuel = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        int[] initialFuel = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
         int[] consumptionLevel = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
         int[] fuelNeeded = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
 
         ArrayDeque<Integer> fuelQueue = new ArrayDeque<>();
         ArrayDeque<Integer> consumptionLevelQueue = new ArrayDeque<>();
         ArrayDeque<Integer> altitudesMark = new ArrayDeque<>();
-        fillStack(fuelQueue, fuel);
-        fillQueue(consumptionLevelQueue, consumptionLevel);
-        fillQueue(altitudesMark, fuelNeeded);
+        fillQueue(fuelQueue, initialFuel);
+        fillStack(consumptionLevelQueue, consumptionLevel);
+        fillStack(altitudesMark, fuelNeeded);
 
         int altitude = 0;
-        Map<String, List<Integer>> altitudeList = new TreeMap<>();
 
+        List<String> altitudeList = new ArrayList<>();
         while (!fuelQueue.isEmpty() && !consumptionLevelQueue.isEmpty() && !altitudesMark.isEmpty()) {
 
             int tankLevel = fuelQueue.peek();
@@ -29,46 +30,56 @@ public class OffroadChallenge {
             if (tankLevel - currentConsumption >= neededFuel) {
                 altitude++;
                 System.out.println("John has reached: Altitude " + altitude);
-                altitudeList.put("Altitude", new ArrayList<>());
-                altitudeList.get("Altitude").add(altitude);
+                String altitudeToAdd = "Altitude " + altitude;
+                altitudeList.add(altitudeToAdd);
                 //remove elements
                 fuelQueue.poll();
                 consumptionLevelQueue.pop();
                 altitudesMark.pop();
-            } else {
+            } else if (tankLevel - currentConsumption < neededFuel) {
                 fuelQueue.poll();
                 consumptionLevelQueue.pop();
                 altitudesMark.pop();
 
                 altitude++;
                 System.out.println("John did not reach: Altitude " + altitude);
+
                 break;
 
             }
 
         }
-        if (altitudesMark.isEmpty()){
+
+        if (altitudesMark.isEmpty()) {
             System.out.println("John has reached all the altitudes and managed to reach the top!");
-        }else {
+        }else{
             System.out.println("John failed to reach the top.");
-            if (fuelQueue.isEmpty() && altitude < 1) {
-                System.out.println("John didn't reach any altitude.");
-            }else {
+            if ( !altitudeList.isEmpty()) {
                 System.out.print("Reached altitudes: ");
+            } else {
+                System.out.println("John didn't reach any altitude.");
             }
+            printList(altitudeList);
         }
-
-
-
+    }
+    private static String convertString (String output,List <String> list){
 
     }
 
-    private static void fillStack(ArrayDeque<Integer> fuelQueue, int[] fuel) {
-        Arrays.stream(fuel).forEach(fuelQueue::offer);
+    private static void printList(List<String> altitudeList) {
+
+        String output = altitudeList.stream().map(String::valueOf).collect(Collectors.joining(", "));
+
+        System.out.println(output);
+    }
+
+
+    private static void fillQueue(ArrayDeque<Integer> fuelQueue, int[] fuel) {
+        Arrays.stream(fuel).forEach(fuelQueue::push);
 
     }
 
-    private static void fillQueue(ArrayDeque<Integer> stack, int[] arr) {
-        Arrays.stream(arr).forEach(stack::push);
+    private static void fillStack(ArrayDeque<Integer> stack, int[] arr) {
+        Arrays.stream(arr).forEach(stack::offer);
     }
 }
